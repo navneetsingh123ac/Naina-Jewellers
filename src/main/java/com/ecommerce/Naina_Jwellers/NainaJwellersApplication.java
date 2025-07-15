2fa242cd85362915a989831b3845e19b94116ad4
Navneet Singh <105372234+navneetsingh123ac@users.noreply.github.com>
2025-07-15 15:23:52 +0530

    Let's Begin

---
+++ src/main/java/com/ecommerce/Naina_Jwellers/NainaJwellersApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.Naina_Jwellers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NainaJwellersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NainaJwellersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/Naina_Jwellers/NainaJwellersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.Naina_Jwellers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NainaJwellersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
