3f5e440d510e497581f84d642c2d996fc22262a9
Navneet Singh <105372234+navneetsingh123ac@users.noreply.github.com>
2025-07-27 15:32:05 +0530

    connect database

--- src/main/java/com/ecommerce/Naina_Jwellers/Model/Category.java
+++ src/main/java/com/ecommerce/Naina_Jwellers/Model/Category.java
@@ -1,27 +1,25 @@
 package com.ecommerce.Naina_Jwellers.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Entity(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @NotBlank(message = "Category name must not be blank")
     private String name;
 
-    public Category(long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-}
+   }
--- src/main/java/com/ecommerce/Naina_Jwellers/Service/implement/CategoryServiceImplement.java
+++ src/main/java/com/ecommerce/Naina_Jwellers/Service/implement/CategoryServiceImplement.java
@@ -2,6 +2,8 @@ package com.ecommerce.Naina_Jwellers.Service.implement;
 
 import com.ecommerce.Naina_Jwellers.Model.Category;
 import com.ecommerce.Naina_Jwellers.Service.CategoryService;
+import com.ecommerce.Naina_Jwellers.repository.CategoryRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
@@ -14,40 +16,36 @@ import java.util.Optional;
 @Service
 public class CategoryServiceImplement implements CategoryService {
 
-    private List<Category> categories = new ArrayList<Category>();
+    //private List<Category> categories = new ArrayList<Category>();
+
+    @Autowired
+    private CategoryRepo categoryRepo;
+
     @Override
     public List<Category> getAllCategories() {
-        return categories;
+        return categoryRepo.findAll();
     }
 
     @Override
     public void addCategory(Category category) {
-        categories.add(category);
+        categoryRepo.save(category);
     }
 
     @Override
     public String deleteCategory(Long id) {
-        Category category = categories.stream()
-                .filter(c -> id.equals(c.getId()))
-                .findFirst()
+        Category category = categoryRepo.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
-
-        categories.remove(category);
-        return "category with categoryId: " + id + " was deleted";
+        categoryRepo.delete(category);
+        return "Category with ID: " + id + " was deleted";
     }
 
     @Override
     public Category updateCategory(Category category, Long categoryId) {
-        Optional<Category> optionalCategory = categories.stream()
-                .filter(c -> categoryId.equals(c.getId()))
-                .findFirst();
-        if (optionalCategory.isPresent()) {
-            Category existingCategory = optionalCategory.get();
-            existingCategory.setName(category.getName());
-            return existingCategory;
-        } else{
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found");
-        }
-
+        Category existingCategory = categoryRepo.findById(categoryId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
+        existingCategory.setName(category.getName());
+        return categoryRepo.save(existingCategory);
     }
+
+
 }
